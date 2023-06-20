45ae73098f1c7da8a20f67975bd7308bec894927
Harun TAŞCI <haruntasci77@gmail.com>
2023-06-20 19:59:40 +0300

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home';
+import { LoginComponent, RegisterComponent } from './account';
+
+const routes: Routes = [
+
+  {path:'', component: HomeComponent},
+  {path:'account/login', component: LoginComponent},
+  {path:'account/register', component:RegisterComponent},
+  {path:'**', redirectTo:''}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { LoginComponent, RegisterComponent } from './account';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
